e03bc8669a1d659cca5c98f18d2faf79f7913d9b
root <root@de1soclinux.(none)>
2023-12-28 13:48:22 +0000

    more fixes

--- vga/everest_image.c
+++ vga/everest_image.c
@@ -74,26 +74,6 @@ int main(void)  {
     // ========================================================================
 
 	FILE *pic_pointer;
-    pic_pointer=fopen("everest_16x16.bin", "rb");
-    if (!pic_pointer) { 
-      printf("Unable to open picture file!");    
-	  return 1;
-    }
-    char pic_buffer[2];
-	int i=0;
-	while (!feof(pic_pointer)) {
-        fread(pic_buffer, 2, 1, pic_pointer);
-        //printf("%s", pic_buffer);
-		vga_pixel_ptr =(unsigned int *)(vga_pixel_virtual_base+(2*i));
-		*vga_pixel_ptr = pic_buffer ;
-		printf("%s, %d \n", pic_buffer, i);
-		i++;
-    }
-    
-	// ========================================================================
-    // ========================================================================
-
-	FILE *pic_pointer;
         pic_pointer=fopen("everest_32x32.bin", "rb");
         if (!pic_pointer) { 
           printf("Unable to open picture file!");    
